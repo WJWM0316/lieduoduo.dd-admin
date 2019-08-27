b131d318ac06bc89dc4164dca727d78cc58cd96f
lyuquan <1831615701@qq.com>
2019-08-27 18:05:25 +0800

    浏览次数，和浏览人数

--- src/pages/24h/24h.vue
+++ src/pages/24h/24h.vue
@@ -146,20 +146,20 @@
           {{scope.row.seatsNum - scope.row.applyNum - scope.row.natureApplyNum}}
         </template>
       </el-table-column>
-      <!-- <el-table-column
+      <el-table-column
         prop="seatsNum"
         label="浏览人数">
         <template slot-scope="scope">
-          {{scope.row.seatsNum - scope.row.applyNum - scope.row.natureApplyNum}}
+          {{scope.row.uv}}
         </template>
       </el-table-column>
       <el-table-column
         prop="seatsNum"
         label="浏览次数">
         <template slot-scope="scope">
-          {{scope.row.seatsNum - scope.row.applyNum - scope.row.natureApplyNum}}
+          {{scope.row.pv}}
         </template>
-      </el-table-column> -->
+      </el-table-column>
       <el-table-column
         prop="isOnline"
         label="上架状态">
